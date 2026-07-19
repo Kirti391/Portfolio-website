adcd5038c8d37db619a606409f873003279ecaaa
Kirti <162712321+Kirti391@users.noreply.github.com>
2026-07-19 18:06:35 +0530

    feat:footer heading

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,16 @@
 function Footer(){
     return(
-        <footer className="bg-[#070B14] w-full px-6 md:px-16 lg:px-28">
+        <footer className="bg-[#070B14] w-full px-6 md:px-16 lg:px-28 py-16">
          
-
+          <div className="max-w-6xl mx-auto ">
+            <h2 className="text-center font-bold text-3xl md:text-4xl font-bold tracking-tight text-white">Thanks for visiting.</h2>
+            <p className="text-center text-gray-400 mt-4 leading-8 mx-auto max-w-xl">I appreciate you taking the time to explore my portfolio.</p>
+            <div className="flex items-center justify-center gap-4">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </footer>
     )
 }
